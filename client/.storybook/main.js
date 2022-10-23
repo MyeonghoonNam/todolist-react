39505b6120c9408b0ae6bf01bbf189fc0bdc6323
MyeongHoonNam <codenmh0822@naver.com>
2022-10-24 00:19:26 +0900

    feat #7: 절대경로 추가

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -20,6 +20,7 @@ module.exports = {
 				alias: {
 					'@assets': path.resolve(__dirname, '../src/assets'),
 					'@components': path.resolve(__dirname, '../src/components'),
+					'@hooks': path.resolve(__dirname, '../src/hooks'),
 				},
 			},
 		};
