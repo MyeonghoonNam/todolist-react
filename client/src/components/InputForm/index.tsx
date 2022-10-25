91599e70f71f315ed5e037ad0add27f204e8a632
MyeongHoonNam <codenmh0822@naver.com>
2022-10-25 21:08:06 +0900

    feat #11: InputForm 컴포넌트 UI 구현

---
+++ client/src/components/InputForm/index.tsx
@@ -0,0 +1,74 @@
+import styled from '@emotion/styled';
+import { FaSearch, FaPlusCircle } from 'react-icons/fa';
+import font from '@assets/font';
+import color from '@assets/color';
+import { css } from '@emotion/react';
+
+const Container = styled.form`
+	position: relative;
+	width: 100%;
+	margin-bottom: 20px;
+	display: flex;
+	border-radius: calc(0.5 * 100px);
+	box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.38);
+	justify-content: space-evenly;
+
+	&:hover {
+		box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.58);
+	}
+`;
+
+const Icon = styled.i`
+	display: flex;
+	align-items: center;
+	padding-left: 5px;
+`;
+
+const Input = styled.input`
+	${font.regular};
+	width: 85%;
+	padding-right: 5px;
+	padding-left: 5px;
+	border-radius: calc(0.5 * 100px);
+	background-color: transparent;
+	height: 45px;
+	outline: none;
+	border: none;
+`;
+
+const Button = styled.button`
+	background: transparent;
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	height: 45px;
+	outline: none;
+	border: none;
+`;
+
+const PlusButtonStyle = css`
+	color: ${color.plus_button};
+	font-size: 20px;
+
+	&:hover {
+		opacity: 0.5;
+	}
+`;
+
+const InputForm = () => {
+	return (
+		<Container>
+			<Icon>
+				<FaSearch />
+			</Icon>
+
+			<Input type="text" autoFocus />
+
+			<Button type="submit" css={PlusButtonStyle}>
+				<FaPlusCircle />
+			</Button>
+		</Container>
+	);
+};
+
+export default InputForm;
