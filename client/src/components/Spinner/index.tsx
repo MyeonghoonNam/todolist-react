ea683497b855017e984d7dba8727e83e38e6736b
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-10-25 17:42:24 +0900

    [Feature/#9] Spinner 컴포넌트 구현 (#10)
    * chore #9: react-icons install
    
    * feat #9: Spinner 컴포넌트 구현
    
    * feat #9: Spinner 컴포넌트 스토리북 구현

---
+++ client/src/components/Spinner/index.tsx
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+import { FaSpinner } from 'react-icons/fa';
+
+const SpinnerStyle = css`
+	font-size: 20px;
+	animation: spin 2s linear infinite;
+	display: flex;
+	align-self: center;
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+`;
+
+const Spinner = () => {
+	return <FaSpinner css={SpinnerStyle} />;
+};
+
+export default Spinner;
---
+++ client/src/stories/Spinner.stories.tsx
@@ -0,0 +1,10 @@
+import Spinner from '@components/Spinner';
+
+export default {
+	title: 'Components/Spinner',
+	component: Spinner,
+};
+
+export const Default = () => {
+	return <Spinner />;
+};
