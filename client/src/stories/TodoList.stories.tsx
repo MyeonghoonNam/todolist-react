db0d594a19996da8f27298c37b3654ef4b95b1fb
MyeongHoonNam <codenmh0822@naver.com>
2022-10-26 18:55:37 +0900

    feat #13: TodoList 컴포넌트 스토리북 구현

---
+++ client/src/stories/TodoList.stories.tsx
@@ -0,0 +1,21 @@
+import TodoList from '@components/TodoList';
+import TodosProvider from '@contexts/TodosProvider';
+import uuid from 'react-uuid';
+
+export default {
+	title: 'Components/TodoList',
+	component: TodoList,
+};
+
+export const Default = () => {
+	return (
+		<TodosProvider
+			initialState={[
+				{ id: uuid(), content: 'TypeScript', complete: false },
+				{ id: uuid(), content: 'JavaScript', complete: true },
+			]}
+		>
+			<TodoList />
+		</TodosProvider>
+	);
+};
