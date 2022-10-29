b715dd2eafdb77fd33e27dc11031a7c3ae80d804
MyeongHoonNam <codenmh0822@naver.com>
2022-10-29 22:12:39 +0900

    feat #22: api wrapper 구현

---
+++ client/src/api/index.tsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+const baseInstance = axios.create({
+	baseURL,
+});
+
+baseInstance.interceptors.response.use((res) => {
+	return res.data;
+});
+
+const api = {
+	get: (url: string, config?: object) => baseInstance.get(url, config),
+	post: (url: string, data: object, config?: object) =>
+		baseInstance.post(url, data, config),
+	put: (url: string, data: object, config?: object) =>
+		baseInstance.put(url, data, config),
+	patch: (url: string, data: object, config?: object) =>
+		baseInstance.patch(url, data, config),
+	delete: (url: string, config?: object) => baseInstance.delete(url, config),
+};
+
+export default api;
