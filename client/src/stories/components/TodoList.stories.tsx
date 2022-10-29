4847e0432126cba0584fb262d4e6f95a190d8345
MyeongHoonNam <codenmh0822@naver.com>
2022-10-29 21:56:50 +0900

    remove #22: 불필요 코드 제거

--- client/src/stories/components/TodoList.stories.tsx
+++ client/src/stories/components/TodoList.stories.tsx
@@ -1,6 +1,5 @@
 import TodoList from '@components/TodoList';
 import TodosProvider from '@contexts/TodosContext';
-import uuid from 'react-uuid';
 
 export default {
 	title: 'Components/TodoList',
@@ -9,20 +8,7 @@ export default {
 
 export const Default = () => {
 	return (
-		<TodosProvider
-			initialState={[
-				{
-					id: uuid(),
-					title: 'TypeScript',
-					complete: false,
-				},
-				{
-					id: uuid(),
-					title: 'JavaScript',
-					complete: true,
-				},
-			]}
-		>
+		<TodosProvider>
 			<TodoList />
 		</TodosProvider>
 	);
