6aee4313d2580eb257c0ed049f95c9ab765272f2
MyeongHoonNam <codenmh0822@naver.com>
2022-11-07 20:33:58 +0900

    feat #44: user 로그인, 회원가입 라우터 구현

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,7 @@ import createError from 'http-errors';
 import { StatusCodes } from 'http-status-codes';
 
 import todoRouter from './routes/todoRouter';
+import userRouter from './routes/userRouter';
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/todos', todoRouter);
+app.use('/users', userRouter);
 
 app.use((req, res, next) => {
 	next(createError(404));
