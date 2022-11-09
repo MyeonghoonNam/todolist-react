b9d4e7a2667c3b0a32d0673dd053fb37dd53587a
MyeongHoonNam <codenmh0822@naver.com>
2022-11-09 18:16:03 +0900

    feat #48: user auth 라우팅 구현

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/login', authController.login);
 router.post('/signup', authController.signUp);
+router.get('/auth', authController.auth);
 
 export default router;
