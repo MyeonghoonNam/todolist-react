f0b9e8a24570f4be8a6ad2b322df1d13f66eaaa9
MyeongHoonNam <codenmh0822@naver.com>
2022-11-10 21:49:00 +0900

    feat #48: 인증 관련 에러 메시지 지정

--- server/src/utils/validator.ts
+++ server/src/utils/validator.ts
@@ -40,3 +40,7 @@ export const TODO_VALIDATION_ERRORS = {
 	TODO_SOMETHING_WRONG: 'todo를 찾는 도중 문제가 생겼습니다',
 	INVALID_VALUE: 'input을 다시 확인해주세요',
 };
+
+export const USER_AUTHORIZATION_ERROS = {
+	TOKEN_NOT_FOUND: '토큰이 존재하지 않습니다.',
+};
