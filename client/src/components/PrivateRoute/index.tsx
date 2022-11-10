650c878913933ed250281d84c078c30851857d26
MyeongHoonNam <codenmh0822@naver.com>
2022-11-10 21:44:12 +0900

    feat #48: PrivateRoute 구현

---
+++ client/src/components/PrivateRoute/index.tsx
@@ -0,0 +1,11 @@
+import { RootState } from '@store/.';
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
+
+const PrivateRoute = () => {
+	const { isAuth } = useSelector((state: RootState) => state.users);
+
+	return isAuth ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
