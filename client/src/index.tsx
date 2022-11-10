07ad9ed669f236a9e7560683f24c4c0f701835d4
MyeongHoonNam <codenmh0822@naver.com>
2022-11-10 21:44:50 +0900

    feat #48: 인증 여부에 따른 라우팅 권한 제어

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { CookiesProvider } from 'react-cookie';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from '@styles/global';
+import { store, persistor } from './store';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +19,15 @@ const root = ReactDOM.createRoot(
 
 root.render(
 	<React.StrictMode>
-		<App />
+		<CookiesProvider>
+			<Provider store={store}>
+				<PersistGate persistor={persistor}>
+					<BrowserRouter>
+						<GlobalStyle />
+						<App />
+					</BrowserRouter>
+				</PersistGate>
+			</Provider>
+		</CookiesProvider>
 	</React.StrictMode>,
 );
