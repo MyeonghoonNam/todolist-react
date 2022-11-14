8a224493c7681fbaf85f9b61507ce833953bb145
MyeongHoonNam <codenmh0822@naver.com>
2022-11-14 16:35:26 +0900

    feat #54: cookie util 함수 구현

--- client/src/utils/cookies.ts
+++ client/src/utils/cookies.ts
@@ -2,10 +2,14 @@ import { Cookies } from 'react-cookie';
 
 const cookie = new Cookies();
 
-export const setCookie = (name: string, value: object, options: object) => {
+export const setCookie = (name: string, value: object, options?: object) => {
 	return cookie.set(name, value, { ...options });
 };
 
 export const getCookie = (name: string) => {
 	return cookie.get(name);
+};
+
+export const removeCookie = (name: string) => {
+	return cookie.remove(name);
 };
