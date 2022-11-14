801d0275c3b7b43a9a9515d789f08b0b2f5f415e
MyeongHoonNam <codenmh0822@naver.com>
2022-11-14 16:37:23 +0900

    feat #54: interceptor를 활용한 token refresh 적용

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,3 +1,5 @@
+import { getCookie, removeCookie, setCookie } from '@utils/cookies';
+
 import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_API_URL;
@@ -7,19 +9,92 @@ const baseInstance = axios.create({
 	withCredentials: true,
 });
 
-baseInstance.interceptors.request.use((req) => {
-	const token = localStorage.getItem('token');
+let lock = false;
+let subscribers: ((token: string) => void)[] = [];
 
-	if (req.headers && token) {
-		req.headers.Authorization = `Bearer ${token}`;
+const resource = 'users';
+const refreshURL = `${baseURL}/${resource}/refresh`;
+
+const subscribeTokenRefresh = (cb: (token: string) => void) => {
+	subscribers.push(cb);
+};
+
+const onRrefreshed = (token: string) => {
+	subscribers.forEach((cb) => cb(token));
+};
+
+const onRefresh = async (): Promise<string | void> => {
+	try {
+		const { data: token } = await api.get(refreshURL);
+		const { accessToken, refreshToken } = token;
+
+		lock = false;
+		onRrefreshed(accessToken);
+		subscribers = [];
+		localStorage.setItem('token', accessToken);
+
+		if (refreshToken !== null) {
+			setCookie('token', refreshToken);
+		}
+
+		return accessToken;
+	} catch (e) {
+		lock = false;
+		subscribers = [];
+		localStorage.removeItem('token');
+		removeCookie('token');
+	}
+};
+
+baseInstance.interceptors.request.use((config) => {
+	if (!config.headers) return config;
+
+	let token: string | null = null;
+
+	if (config.url === refreshURL) {
+		token = getCookie('token');
+	} else {
+		token = localStorage.getItem('token');
 	}
 
-	return req;
+	if (token !== null) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+
+	return config;
 });
 
-baseInstance.interceptors.response.use((res) => {
-	return res;
-});
+baseInstance.interceptors.response.use(
+	(res) => res,
+	async (err) => {
+		const {
+			config,
+			response: { status },
+		} = err;
+		const originalRequest = config;
+
+		if (config.url === refreshURL || status !== 401) return Promise.reject(err);
+
+		if (lock) {
+			return new Promise((resolve) => {
+				subscribeTokenRefresh((token: string) => {
+					originalRequest.headers.Authorization = `Bearer ${token}`;
+					resolve(axios(originalRequest));
+				});
+			});
+		}
+
+		lock = true;
+		const accessToken = await onRefresh();
+
+		if (typeof accessToken === 'string') {
+			config.headers.Authorization = `Bearer ${accessToken}`;
+			return axios(config);
+		}
+
+		return Promise.reject(err);
+	},
+);
 
 const api = {
 	get: (url: string, config?: object) => baseInstance.get(url, config),
