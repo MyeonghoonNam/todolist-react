ed8bde4ea14ef54e30e45350a257eb66947cc72d
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2022-12-30 19:19:20 +0900

    [Feature/#73] Axios Interceptor Customizing (#74)
    * feat #73: axios interceptor 타입 상속
    
    * feat #73: 요청/응답에 대한 선행/후행 로직 분리
    
    * feat #73: interceptor 생성 로직 구현

---
+++ client/src/api/config/index.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import interceptor from './interceptor';
+
+import type { Instance } from './types';
+
+const createInterceptor = () => {
+	const instance: Instance = axios.create({
+		baseURL: process.env.REACT_APP_API_URL,
+	});
+
+	interceptor(instance);
+
+	return instance;
+};
+
+const config = createInterceptor();
+
+export default config;
