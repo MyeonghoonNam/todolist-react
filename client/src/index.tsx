6f2e0abf440108dc26dc83b6c0544436a4a443fc
MyeongHoonNam <codenmh0822@naver.com>
2023-01-01 02:17:20 +0900

    chore #75: react-query v4 업그레이드

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,27 +3,27 @@ import ReactDOM from 'react-dom/client';
 import { CookiesProvider } from 'react-cookie';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from '@styles/global';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools';
 
 import App from './App';
 
 const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement,
 );
 
 const queryClient = new QueryClient();
 
 root.render(
-	<React.StrictMode>
-		<CookiesProvider>
-			<QueryClientProvider client={queryClient}>
-				<BrowserRouter>
-					<GlobalStyle />
-					<App />
-					<ReactQueryDevtools initialIsOpen={false} />
-				</BrowserRouter>
-			</QueryClientProvider>
-		</CookiesProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </CookiesProvider>
+  </React.StrictMode>,
 );
