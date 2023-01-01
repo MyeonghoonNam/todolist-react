95c0fac07f189833f026af60f9215a2d480f81f0
MyeongHoonNam <codenmh0822@naver.com>
2023-01-01 17:12:23 +0900

    refactor #75: eslint rule 적용

--- server/src/interfaces/users.ts
+++ server/src/interfaces/users.ts
@@ -1,9 +1,9 @@
 export interface User {
-	id: string;
-	email: string;
-	password: string;
-	createdAt: string;
-	token: string;
+  id: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  token: string;
 }
 
 export type UserInput = Pick<User, 'email' | 'password'>;
