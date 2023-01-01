f61220fb594499524c505004ee908cfac30b30fa
MyeongHoonNam <codenmh0822@naver.com>
2023-01-02 00:51:24 +0900

    refactor #75: TodoList UI 로직 적용

--- client/src/components/TodoList/index.tsx
+++ client/src/components/TodoList/index.tsx
@@ -1,17 +1,15 @@
 import styled from '@emotion/styled';
 import TodoItem from '@components/TodoItem';
-import { useEffect } from 'react';
+import { useTodos } from '@queries/index';
 
 const TodoList = () => {
-  useEffect(() => {
-    // dispatch(getTodoList({ userId: user.id }));
-  }, []);
+  const { data: todos } = useTodos();
 
   return (
     <Container>
-      {/* {todos.map(({ id, title, complete }) => (
-				<TodoItem key={id} id={id} title={title} complete={complete} />
-			))} */}
+      {todos?.map(({ id, title, complete }) => (
+        <TodoItem key={id} id={id} title={title} complete={complete} />
+      ))}
     </Container>
   );
 };
