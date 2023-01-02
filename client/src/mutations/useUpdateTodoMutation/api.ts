8ab25db40acb4a80ae262eab60d684d2f8e445e5
MyeongHoonNam <codenmh0822@naver.com>
2023-01-02 23:06:18 +0900

    refactore #81: RESOURCE 개별 적용

--- client/src/mutations/useUpdateTodoMutation/api.ts
+++ client/src/mutations/useUpdateTodoMutation/api.ts
@@ -1,9 +1,9 @@
 import { api } from '@api/index';
 
-import { RESOURCE } from '@queries/useTodosQuery/api';
-
 import type { Todo } from '@interfaces/index';
 import type { TodoUpdateInput } from './types';
+
+const RESOURCE = '/todos';
 
 export const patchTodo = async (id: string, data: TodoUpdateInput) =>
   api.patch<Todo>(`${RESOURCE}/${id}`, data);
--- client/src/queries/useTodosQuery/api.ts
+++ client/src/queries/useTodosQuery/api.ts
@@ -1,7 +1,7 @@
 import { api } from '@api/index';
 import type { Todo } from '@interfaces/index';
 
-export const RESOURCE = '/todos';
+const RESOURCE = '/todos';
 
 export const getTodos = async (userId?: string) =>
   api.get<Todo[]>(`${RESOURCE}`, { params: { userId } });
