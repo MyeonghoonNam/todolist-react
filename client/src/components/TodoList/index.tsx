68f1719a9b0ea81747ff3a43f4f54decd666d7be
MyeonghoonNam <57757719+MyeonghoonNam@users.noreply.github.com>
2023-01-02 14:16:39 +0900

    [Feature/#78] useTodos import/no-cycle 문제 해결 (#79)
    * refactor #78: useUser hook 분리
    
    * refactor #78: useTodos 조회 비지니스 로직 수정

--- client/src/components/TodoList/index.tsx
+++ client/src/components/TodoList/index.tsx
@@ -1,9 +1,13 @@
 import styled from '@emotion/styled';
 import TodoItem from '@components/TodoItem';
-import { useTodos } from '@queries/index';
+import { useTodos, useUser } from '@queries/index';
 
 const TodoList = () => {
-  const { data: todos } = useTodos();
+  const {
+    query: { data: user },
+  } = useUser();
+
+  const { data: todos } = useTodos(user?.id);
 
   return (
     <Container>
